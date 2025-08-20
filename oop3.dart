77226b8677a56d339ea95cda40392f5e7131e62c
Sourav Bosu <121472055+Sou963@users.noreply.github.com>
2025-08-20 18:33:40 +0600

    Add files via upload

---
+++ oop3.dart
@@ -0,0 +1,15 @@
+class sourav{
+     abc(String need){
+      print(need);
+   }
+    alok(int adnumber){
+    print(adnumber);
+   }
+}
+class ossim extends sourav{
+}
+void main(){
+  var n=new sourav();
+  n.abc("taka");
+  n.alok(500);
+}
