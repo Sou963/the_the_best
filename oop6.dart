b9655c04233c82e86494567f9e7baf5a8175a815
Sourav Bosu <121472055+Sou963@users.noreply.github.com>
2025-08-20 18:34:09 +0600

    Add files via upload

---
+++ oop6.dart
@@ -0,0 +1,19 @@
+class sourav{
+ static String name="0";
+ String email="0";
+ int id=0 ;
+
+  ashim()
+  {
+    print("The name is = $name");
+    print("The id is = $id");
+    print("The email is = $email");
+  }
+}
+main(){
+  sourav s= new sourav();
+  sourav.name="Sourav bosu";
+  s.id=300;
+  s.email="souravbosu844@gmail.com";
+  s.ashim();
+  }
