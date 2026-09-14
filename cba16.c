3a2d4fa145f5b7b6abc87626cd41eac01153b3b9
Sourav Bosu <121472055+Sou963@users.noreply.github.com>
2026-09-15 01:14:07 +0600

    Add files via upload

---
+++ cba16.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+main()
+{
+    double x1,y1,x2,y2,d;
+    scanf("%lf%lf%lf%lf",&x1,&y1,&x2,&y2);
+    d=sqrt(pow(x2-x1,2)+pow(y2-y1,2));
+    printf("%0.4lf\n",d);
+    return 0;
+
+}
